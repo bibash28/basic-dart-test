5eca661f5128ce9e5869bece4de633caa49d22b7
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-24 12:29:48 +0545

    basic dart test

---
+++ README.md
@@ -0,0 +1,11 @@
+Calculator app testing
+
+To run : dart test
+
+Reference: 
+
+https://github.com/jorgecoca/calculator_app
+https://verygood.ventures/blog/guide-to-flutter-testing
+https://www.youtube.com/playlist?list=PLprI2satkVdFwpxo_bjFkCxXz5RluG8FY
+
+
---
+++ bin/testing.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+  print(Calculator().add(2, 2));
+}
+
+class Calculator {
+  double add(double a, double b) => a + b;
+
+  double subtract(double a, double b) => a - b;
+
+  double multiply(double a, double b) => a * b;
+
+  double divide(double a, double b) {
+    if (b == 0) throw ArgumentError('You cannot divide by zero');
+    return a / b;
+  }
+
+  Future<double> powerOfTwo(double a) {
+    return Future.delayed(const Duration(seconds: 1), () => a * a);
+  }
+
+  Stream<double> pi() => Stream.fromIterable([3, 3.1, 3.14, 3.141, 3.1415]);
+}
